b20f9d6233b8fbf6359950136a4ccec1e5c99f9c
0342234 <yarikbosenko@gmail.com>
2018-03-02 17:39:04 +0200

    commit4

--- CCBank/Models/DataSortMethods.swift
+++ CCBank/Models/DataSortMethods.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import CoreData
+
+struct DataSort {
+    static func sortDataSource(data: [CurrenciesAPIModel], completion: @escaping (([CurrenciesAPIModel], [CurrenciesAPIModel] )) -> Void) {
+        
+        var favoriteData: [CurrenciesAPIModel] = [CurrenciesAPIModel]()
+        var notFavoriteData: [CurrenciesAPIModel] = data
+        var favoriteCurrencies: [String] = []
+        
+        let fetchedRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "FavoriteCurrencies")
+        
+        let moc = PersistenceService.context
+        do {
+            let results = try moc.fetch(fetchedRequest)
+            for result in results as! [FavoriteCurrencies] {
+                favoriteCurrencies.append(result.currencies )
+            }
+        } catch  {
+            print(error)
+        }
+        
+        if favoriteCurrencies.isEmpty {
+            favoriteData = []
+            return
+        }
+        
+        for favoriteCurrency in favoriteCurrencies {
+            for (index, object) in notFavoriteData.enumerated() where object.name == favoriteCurrency {
+                let removedObject = notFavoriteData.remove(at: index)
+                favoriteData.append(removedObject)
+            }
+        }
+        completion((favoriteData, notFavoriteData))
+
+    }
+}
